9605a0d23b459829b2b4bc07b1bb2fce5407950c
saradhi katireddy <115570998+saradhikatireddy@users.noreply.github.com>
2024-04-09 20:37:42 +0530

    not work upload program

--- venv/boto.py
+++ venv/boto.py
@@ -5,7 +5,7 @@ import os
 s3_client = boto3.client('s3', region_name='us-east-2')
 
 # Specify the bucket name
-bucket_name = 'saradhi-bucket-2'
+bucket_name = 'saradhi-bucket-4'
 
 # Create the S3 bucket
 s3_client.create_bucket(
@@ -16,3 +16,11 @@ s3_client.create_bucket(
 )
 
 print(f'S3 bucket "{bucket_name}" created successfully.')
+
+# Upload the data folder to the bucket
+data_folder = 'handsign\Data.png'
+for root, dirs, files in os.walk(data_folder):
+    for file in files:
+        file_path = os.path.join(root, file)
+        s3_client.upload_file(file_path, bucket_name, file)
+        print('Data folder uploaded successfully.')
